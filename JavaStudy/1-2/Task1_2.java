1fa0bf5dcd7ee46a4a63012e34e03616cb72c27c
yukaokazaki622 <75303790+yukaokazaki622@users.noreply.github.com>
2020-12-14 20:43:47 +0900

    課題提出テスト
    Task1_2の提出

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -15,15 +15,19 @@ public class Task1_2 {
         // ①「JAPAN」、「AMERICA」、「KOREA」を要素の値（初期値）とする配列 countryを作成しなさい。
         String[] country = new String[3];
         // ② ①で作成した配列の要素数を出力してください。
-        System.out.println("配列countryの要素数..." + country.length);
+        System.out.println(country.length);
         /* ③下記の値を保持した、要素数3のStringクラスの配列strArrayを作成しなさい。
          *   1番目（先頭）の要素に 「りんご」 を代入
          *   2番目の要素に 「もも」 を代入
          *   3番目の要素に 「ぶどう」 を代入
          */
         String[] strArray = new String[3];
+        strArray[0] = "りんご";
+        strArray[1] = "もも";
+        strArray[2] = "ぶどう";
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
-        System.out.println(strArray[1] = "もも");
+        System.out.println(strArray[1]);
+    
         /* ⑤下記の処理について、何をしているのかコメントを記入してください。
          *  [配列intArrayの要素数...5;]
          */
