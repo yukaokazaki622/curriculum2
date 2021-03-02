4a15aa221827ee9aefb5dfe46407675db0ad9f9c
yukaokazaki622 <75303790+yukaokazaki622@users.noreply.github.com>
2021-03-02 21:40:29 +0900

    2-6課題提出
    2-6の提出

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -36,12 +36,13 @@ public class Study {
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
 
+        calendar.add(Calendar.YEAR, 1);
+        calendar.add(Calendar.MONTH, 3);
         calendar.add(Calendar.DATE, 15);
 
-        System.out.println(calendar.get(Calendar.YEAR) + 1);
-        System.out.println(calendar.get(Calendar.MONTH) + 2);
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH));
         System.out.println(calendar.get(Calendar.DATE));
-
 
 
 
